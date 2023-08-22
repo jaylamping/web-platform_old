03a505fa0c3ab76f4217c02bbaf3631e66691f96
jaylamping <35737718+jaylamping@users.noreply.github.com>
2023-08-21 21:46:12 -0500

    omg that took me so long

---
+++ app/auth/page.jsx
@@ -0,0 +1,9 @@
+const Auth = () => {
+  return (
+    <div className="relative h-full w-full bg-[url('/images/memorial.jpeg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      hi
+    </div>
+  );
+};
+
+export default Auth;
